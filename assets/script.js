29adfad56bfe888c263ebef5d894a70788ad4bbf
Socially Awkward Streams <candice.radam@gmail.com>
2022-10-05 12:46:27 -0500

    CSS and HTML push to github.

--- assets/script.js
+++ assets/script.js
@@ -13,3 +13,22 @@ WHEN all questions are answered or the timer reaches 0
 THEN the game is over
 WHEN the game is over
 THEN I can save my initials and score*/
+
+
+
+var questions = [
+    {What are JavaScript Data types?
+        Number
+        String
+        Boolean
+        All of the above}
+    {}
+    {}
+    {}
+    {}
+    {}
+    {}
+    {}
+    {}
+    {}
+]
