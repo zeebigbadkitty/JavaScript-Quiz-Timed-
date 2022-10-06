36346e455ab676896847f0547e9a90c5806733c6
Socially Awkward Streams <candice.radam@gmail.com>
2022-10-06 12:23:51 -0500

    Made a quiz and leaderboard html, added a start and leaderboard button, made slight changes to CSS, and started adding more to JS.

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,7 @@ WHEN I click the start button
 THEN a timer starts and I am presented with a question
 WHEN I answer a question
 THEN I am presented with another question
-WHEN I answer a question incorrectly
+WHEN I answer a question inanswersly
 THEN time is subtracted from the clock
 WHEN all questions are answered or the timer reaches 0
 THEN the game is over
@@ -18,115 +18,131 @@ THEN I can save my initials and score*/
 var randomQuestion = shuffle(questions)
 var currentQuestion
 
+questionEl = document.getElementById('question-line')
+answerButtonEl = document.getElementsByClassName('button')
+
+
 var questions = [
-    {question:'1.	What are JavaScript Data types?'
+    {question:'1.	What are JavaScript Data types?',
     answers:[
-        {text:'Numbers', correct:false}
-        {text:'Strings', correct:false}
-        {text:'Boolean', correct:false}
-        {text:'All of the Above', correct:true}]
+        {text:'Numbers', answers:false}
+        {text:'Strings', answers:false}
+        {text:'Boolean', answers:false}
+        {text:'All of the Above', answers:true}]
 }
 ]
 var questions = [
-    {question:'2.	What company developed JavaScript? '
+    {question:'2.	What company developed JavaScript? ',
     answers:[
-        {text:'IBM', correct:false}
-        {text:'Oracle', correct:false}
-        {text:'Netscape', correct:true}
-        {text:'Microsoft', correct:false}]
+        {text:'IBM', answers:false}
+        {text:'Oracle', answers:false}
+        {text:'Netscape', answers:true}
+        {text:'Microsoft', answers:false}]
 }
 ]
 var questions = [
-    {question:'3.	What symbols are used to denote comments in JavaScript? '
+    {question:'3.	What symbols are used to denote comments in JavaScript? ',
     answers:[
-        {text:'/', correct:true}
-        {text:'$', correct:false}
-        {text:'*', correct:false}
-        {text:';', correct:false}]
+        {text:'/', answers:true}
+        {text:'$', answers:false}
+        {text:'*', answers:false}
+        {text:';', answers:false}]
 }
 ]
 var questions = [
-    {question:'4.	What function is used to make conversions? '
+    {question:'4.	What function is used to make conversions? ',
     answers:[
-        {text:'Type', correct:false}
-        {text:'Convert', correct:false}
-        {text:'Log', correct:false}
-        {text:'Parse', correct:true}]
+        {text:'Type', answers:false}
+        {text:'Convert', answers:false}
+        {text:'Log', answers:false}
+        {text:'Parse', answers:true}]
 }
 ]
 var questions = [
-    {question:'5.	Which of the follow is not a valid variable name in JavaScript?'
+    {question:'5.	Which of the follow is not a valid variable name in JavaScript?',
     answers:[
-        {text:'2headers', correct:true}
-        {text:'_first_header', correct:false}   
-        {text:'MyHeader', correct:false}
-        {text:'$myList', correct:false}]
+        {text:'2headers', answers:true}
+        {text:'_first_header', answers:false}   
+        {text:'MyHeader', answers:false}
+        {text:'$myList', answers:false}]
 }
 ]
 var questions = [
-    {question:'6.	Select the HTML tag that is used to link JavaScript files. '
+    {question:'6.	Select the HTML tag that is used to link JavaScript files. ',
     answers:[
-        {text:'<href>', correct:false}
-        {text:'<link>', correct:false}
-        {text:'<!DOCTYPE>', correct:false}
-        {text:'<script>', correct:true}]
+        {text:'<href>', answers:false}
+        {text:'<link>', answers:false}
+        {text:'<!DOCTYPE>', answers:false}
+        {text:'<script>', answers:true}]
 }
 ]
 var questions = [
-    {question:'7.	Using these types of statements is how you test for a specific condition.'
+    {question:'7.	Using these types of statements is how you test for a specific condition.',
     answers:[
-        {text:'', correct:}
-        {text:'', correct:}
-        {text:'', correct:}
-        {text:'', correct:}]
+        {text:'for', answers:false}
+        {text:'if', answers:true}
+        {text:'switch', answers:false}
+        {text:'log', answers:false}]
 }
 ]
 var questions = [
-    {question:'8.	The following method of an array adds and/or removes elements from an array.'
+    {question:'8.	The following method of an array adds and/or removes elements from an array.',
     answers:[
-        {text:'', correct:}
-        {text:'', correct:}
-        {text:'', correct:}
-        {text:'', correct:}]
+        {text:'Join', answers:false}
+        {text:'Shift', answers:false}
+        {text:'Concat', answers:false}
+        {text:'Splice', answers:true}]
 }
 ]
 var questions = [
-    {question:'9.	What does the acronym JSON stand for?'
+    {question:'9.	What does the acronym JSON stand for?',
     answers:[
-        {text:'', correct:}
-        {text:'', correct:}
-        {text:'', correct:}
-        {text:'', correct:}]
+        {text:'Java Standard Output Network', answers:false}
+        {text:'JavaScript Object Notation', answers:true}
+        {text:'JavaScript Output Name', answers:false}
+        {text:'Java Source Open Network', answers:false}]
 }
 ]
 var questions = [
-    {question:'10.	What is the output of the following function?\nvar num = 8;\nvar num = 10;\nconsole.log(num);'
+    {question:'10.	What is the output of the following function?\nvar num = 8;\nvar num = 10;\nconsole.log(num);',
     answers:[
-        {text:'', correct:}
-        {text:'', correct:}
-        {text:'', correct:}
-        {text:'', correct:}]
+        {text:'8', answers:false}
+        {text:'2', answers:false}
+        {text:'80', answers:false}
+        {text:'10', answers:true}]
 }
 ]
 var questions = [
-    {question:'What does the keyword “this” mean in JavaScript?'
+    {question:'What does the keyword “this” mean in JavaScript?',
     answers:[
-        {text:'', correct:}
-        {text:'', correct:}
-        {text:'', correct:}
-        {text:'', correct:}]
+        {text:'It is a variable that contains a value', answers:false}
+        {text:'It references the document', answers:false}
+        {text:'It refers to the current object', answers:true}
+        {text:'None of the above. ', answers:false}]
 }
 ]
 
 
 function startGame() { //This function starts the game, ending with the next question populating.
-randomQuestion = questions.sort()
+randomQuestion = questions.sort(() => Math.random() - 0.5)  //To generate a random question.
+questionEl.inn
 nextQuestion()
 
 }
-function selection(){
+function answerSelection(){ //This function
 
 }
 function nextQuestion(){
 
 }
+
+//Score
+for (var i=0; i<questions.length; i++){
+//Finish the For loop
+if (answers === true)
+//Finish the if statement
+}
+//Timer
+//Start Button
+//See Scores Button
+//High Score
