5aa5488e60e48678323b6dda59dfad0a538bf2be
Socially Awkward Streams <candice.radam@gmail.com>
2022-10-08 20:02:39 -0500

    Broke it. Renamed some things and messed with syntax and created a seperate js to rework questions.

--- assets/script.js
+++ assets/script.js
@@ -1,110 +1,168 @@
 //STARTING VARIABLES//
-var startingScore = 0
-var startingTime = 300
+var startBtn = document.getElementById("start-btn");
+var scoresBtn = document.getElementById("scores-btn");
+var currentScore = document.getElementById("current-score");
+var answersButton = document.getElementById("answer-btns");
+var opt1 = document.querySelector("#opt1"); 
+var opt2 = document.querySelector("#opt2"); 
+var opt3 = document.querySelector("#opt3"); 
+var opt4 = document.querySelector("#opt4"); 
 
 //TIMER//
-var timeEl = document.querySelector("#timer");
-var secondsLeft = 180;
+var timerEl = document.querySelector("#timer");
+var secondsLeft = 90;
 function setTime() {
-  var timerInterval = setInterval(function() {
+  var timerInterval = setInterval(function () {
     secondsLeft--;
     timerEl.innerHTML = secondsLeft;
-    if(secondsLeft === 0) {
+    if (secondsLeft === 0) {
       clearInterval(timerInterval);
       sendMessage();
     }
   }, 1000);
 }
-setTime(); //var body = document.body (Append to this)
+setTime(); 
 
 //START GAME AND TIMER
-//DISPLAY RANDOM QUESTION
+var startingScore = 0;
+var startingTime = 300;
 
- // QUESTIONS AND ANSWERS //
-var questions = [
-    {question:'1.	What are JavaScript Data types?',
-    answers:[
-        {text:'Numbers', answers:false}
-        {text:'Strings', answers:false}
-        {text:'Boolean', answers:false}
-        {text:'All of the Above', answers:true}]
+// QUESTIONS AND ANSWERS ARRAY//
+var quizQuestions = [{
+    question: "What are JavaScript Data types?",
+    answers: [
+      { text: "Numbers", isCorrect: false },
+      { text: "Strings", isCorrect: false },
+      { text: "Boolean", isCorrect: false },
+      { text: "All of the Above", isCorrect: true },
+    ],
+  },
+  {
+    question: "What company developed JavaScript?",
+    answers: [
+      { text: "IBM", isCorrect: false },
+      { text: "Oracle", isCorrect: false },
+      { text: "Netscape", isCorrect: true },
+      { text: "Microsoft", isCorrect: false },
+    ],
+  },
+  {
+    question: "What symbols are used to denote comments in JavaScript? ",
+    answers: [
+      { text: "/", isCorrect: true },
+      { text: "$", isCorrect: false },
+      { text: "*", isCorrect: false },
+      { text: ";", isCorrect: false },
+    ],
+  },
+  {
+    question: "What function is used to make conversions? ",
+    answers: [
+      { text: "Type", isCorrect: false },
+      { text: "Convert", isCorrect: false },
+      { text: "Log", isCorrect: false },
+      { text: "Parse", isCorrect: true },
+    ],
+  },
+  {
+    question: "Which of the follow is not a valid variable name in JavaScript?",
+    answers: [
+      { text: "2headers", isCorrect: true },
+      { text: "_first_header", isCorrect: false },
+      { text: "MyHeader", isCorrect: false },
+      { text: "$myList", isCorrect: false },
+    ],
+  },
+  {
+    question: "Select the HTML tag that is used to link JavaScript files. ",
+    answers: [
+      { text: "<href>", isCorrect: false },
+      { text: "<link>", isCorrect: false },
+      { text: "<!DOCTYPE>", isCorrect: false },
+      { text: "<script>", isCorrect: true },
+    ],
+  },
+  {
+    question:
+      "Using these types of statements is how you test for a specific condition.",
+    answers: [
+      { text: "for", isCorrect: false },
+      { text: "if", isCorrect: true },
+      { text: "switch", isCorrect: false },
+      { text: "log", isCorrect: false },
+    ],
+  },
+  {
+    question:
+      "The following method of an array adds and/or removes elements from an array.",
+    answers: [
+      { text: "Join", isCorrect: false },
+      { text: "Shift", isCorrect: false },
+      { text: "Concat", isCorrect: false },
+      { text: "Splice", isCorrect: true },
+    ],
+  },
+  {
+    question: "What does the acronym JSON stand for?",
+    answers: [
+      { text: "Java Standard Output Network", isCorrect: false },
+      { text: "JavaScript Object Notation", isCorrect: true },
+      { text: "JavaScript Output Name", isCorrect: false },
+      { text: "Java Source Open Network", isCorrect: false },
+    ],
+  },
+  {
+    question:
+      "What is the output of the following function?\nvar num = 8;\nvar num = 10;\nconsole.log(num);",
+    answers: [
+      { text: "8", isCorrect: false },
+      { text: "2", isCorrect: false },
+      { text: "80", isCorrect: false },
+      { text: "10", isCorrect: true },
+    ],
+  },
+  {
+    question: "What does the keyword “this” mean in JavaScript?",
+    answers: [
+      { text: "It is a variable that contains a value", isCorrect: false },
+      { text: "It references the document", isCorrect: false },
+      { text: "It refers to the current object", isCorrect: true },
+      { text: "None of the above. ", isCorrect: false },
+    ],
+  },
+];
+
+
+//DISPLAY RANDOM QUESTION
+var questionLine = document.getElementById("question-line");
+var questionIndex = 0
+
+function currentQuestion() {
+  questionLine.textContent = quizQuestions[questionIndex].question; 
+  for(var i = 0; i<=3; i++){
+    answers[i].textContent = quizQuestions[questionIndex].answers[i].text; 
+    answers[i].addEventListener('click',function() 
+    {
+    questionIndex = questionIndex + 1   
+    currentQuestion();
+    })
+  }
 }
-    {question:'2.	What company developed JavaScript? ',
-    answers:[
-        {text:'IBM', answers:false}
-        {text:'Oracle', answers:false}
-        {text:'Netscape', answers:true}
-        {text:'Microsoft', answers:false}]
-}
-    {question:'3.	What symbols are used to denote comments in JavaScript? ',
-    answers:[
-        {text:'/', answers:true}
-        {text:'$', answers:false}
-        {text:'*', answers:false}
-        {text:';', answers:false}]
-}
-    {question:'4.	What function is used to make conversions? ',
-    answers:[
-        {text:'Type', answers:false}
-        {text:'Convert', answers:false}
-        {text:'Log', answers:false}
-        {text:'Parse', answers:true}]
-}
-    {question:'5.	Which of the follow is not a valid variable name in JavaScript?',
-    answers:[
-        {text:'2headers', answers:true}
-        {text:'_first_header', answers:false}   
-        {text:'MyHeader', answers:false}
-        {text:'$myList', answers:false}]
-}
-    {question:'6.	Select the HTML tag that is used to link JavaScript files. ',
-    answers:[
-        {text:'<href>', answers:false}
-        {text:'<link>', answers:false}
-        {text:'<!DOCTYPE>', answers:false}
-        {text:'<script>', answers:true}]
-}
-    {question:'7.	Using these types of statements is how you test for a specific condition.',
-    answers:[
-        {text:'for', answers:false}
-        {text:'if', answers:true}
-        {text:'switch', answers:false}
-        {text:'log', answers:false}]
-}
-    {question:'8.	The following method of an array adds and/or removes elements from an array.',
-    answers:[
-        {text:'Join', answers:false}
-        {text:'Shift', answers:false}
-        {text:'Concat', answers:false}
-        {text:'Splice', answers:true}]
-}
-    {question:'9.	What does the acronym JSON stand for?',
-    answers:[
-        {text:'Java Standard Output Network', answers:false}
-        {text:'JavaScript Object Notation', answers:true}
-        {text:'JavaScript Output Name', answers:false}
-        {text:'Java Source Open Network', answers:false}]
-}
-    {question:'10.	What is the output of the following function?\nvar num = 8;\nvar num = 10;\nconsole.log(num);',
-    answers:[
-        {text:'8', answers:false}
-        {text:'2', answers:false}
-        {text:'80', answers:false}
-        {text:'10', answers:true}]
-}
-    {question:'What does the keyword “this” mean in JavaScript?',
-    answers:[
-        {text:'It is a variable that contains a value', answers:false}
-        {text:'It references the document', answers:false}
-        {text:'It refers to the current object', answers:true}
-        {text:'None of the above. ', answers:false}]
-}]
+currentQuestion();
+
+
+
 //CHECK ANSWERS
+    op1.innerText = Questions[id].text[0].text;
+    op2.innerText = Questions[id].text[1].text;
+    op3.innerText = Questions[id].text[2].text;
+    op4.innerText = Questions[id].text[3].text;
+
 
 //ADD TO SCORE OR SUBTRACT TIME FOR WRONG ANSWER
 
 //TIME RUNS OUT, GAME IS OVER
 
-//SAVING A HIGH SCORE VIA A FORM/PROMPT 
+//SAVING A HIGH SCORE VIA A FORM/PROMPT
 
 //HIGH SCORE LEADERBOARD
